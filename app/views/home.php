0c0e59c441e12541f73c3cc72e6c618fe3b65a4b
Jeraldine Bolilan <bolilanjeraldinesalilid@gmail.com>
2019-01-21 23:02:48 +0800

    includes update

--- app/views/home.php
+++ app/views/home.php
@@ -5,8 +5,8 @@
 
 	<div class="container-fluid main1">
 		<div class="jumbotron bg-dark mt-3 text-center text-white">
-			<h1>MyStore</h1>
-			<p>Your Favorite Store</p>
+			<h1>Little Miss K</h1>
+			<p>Dresses for You</p>
 		</div> <!-- end jumbo -->
 	</div> <!-- end container -->
 
